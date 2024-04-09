ce979222f27acfc2baedd5c56406f1d982363a21
User850413 <csyou1228@gmail.com>
2024-04-09 13:23:30 +0900

    feat : 새로운 todo 입력
    1. input에 입력 후 추가 버튼 클릭 시 todos State에 추가됨
    2. 추가 동작 실행 시 input 내 value 초기화
    3. enter 입력으로 todos State에 추가되는 기능

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,11 +1,42 @@
 import React from "react";
+import { useState, useRef } from "react";
+
 import "../css/Editor.css";
 
-function Editor() {
+function Editor({ onCreate }) {
+  const [content, setContent] = useState("");
+
+  const onchangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const contentRef = useRef();
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
   return (
     <div className="Editor">
-      <input type="text" placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onChange={onchangeContent}
+        onKeyDown={onKeyDown}
+        type="text"
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 }
