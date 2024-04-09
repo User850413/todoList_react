ad4ce07db61bc5555ac99b485be63406c27a775f
User850413 <csyou1228@gmail.com>
2024-04-09 13:47:36 +0900

    feat : List 내 content 서치

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "./TodoItem";
 import "../css/List.css";
 
-function List() {
+function List({ todos }) {
+  const [search, setSearch] = useState("");
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) => {
+      return todo.content.toLowerCase().includes(search.toLowerCase());
+    });
+  };
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
-      <input type="text" placeholder="검색어를 입력해 주세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        type="text"
+        placeholder="검색어를 입력해 주세요"
+      />
       <div className="todos_wrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
       </div>
     </div>
   );
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../css/TodoItem.css";
 
-function TodoItem() {
+function TodoItem({ id, isDone, content, date }) {
   return (
     <div className="TodoItem">
-      <input type="checkbox" />
-      <span className="content">Todo...</span>
-      <span className="date">Date</span>
+      <input readOnly checked={isDone} type="checkbox" />
+      <span className="content">{content}</span>
+      <span className="date">{new Date(date).toLocaleDateString()}</span>
       <button>삭제</button>
     </div>
   );
